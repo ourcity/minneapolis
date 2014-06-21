0b0e573071d4728ac2168d7a6f2f95557d0a5f79
Benjamin Rosas <ballPointPenguin@gmx.us>
2014-06-21 17:00:38 -0500

    scaffold Map resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :issues
+  resources :maps
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,5 +57,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :issues
 end
