27a1af93ff80eba613a0ee60951356dd1dabf73d
Benjamin Ortega <ben.ortega@gmail.com>
2014-09-13 16:33:12 -0500

    create sms_subscriptions endpoint using GovD TMS payload

--- app/controllers/sms_subscriptions_controller.rb
+++ app/controllers/sms_subscriptions_controller.rb
@@ -9,4 +9,39 @@ class SmsSubscriptionsController < ApplicationController
     end
 
   end
+  
+  before_filter :find_code, :find_user
+
+  def create
+    @codes.each do |code|
+      @sms_user.send(@action, code)
+    end
+    render nothing: true, status: 201
+  end
+
+  protected
+  def render_unknown
+    render text: "'SUBSCRIBE <CODE>' to subscribe. 'UNSUBSCRIBE <CODE>' to unsubscribe. 'STOP' to stop all updates. Find subscription items at #{root_url}.", status: 202
+    false
+  end
+
+  def find_user
+    scope = SmsUser.where(phone: params[:from])
+    if @action=='unsubscribe'
+      render_unknown and return false unless scope.any?
+    end
+    @sms_user = scope.first_or_create
+  end
+
+  def find_code
+    @codes  = params[:sms_body].downcase.split(' ')
+    @action = @codes.shift
+    raise 'foo' unless ['subscribe', 'unsubscribe'].include?(@action)
+    @codes.map(&:upcase!)
+    @action.downcase!
+  rescue => e
+    logger.warn(e)
+    render_unknown
+  end
+
 end
--- spec/controllers/sms_subscriptions_controller_spec.rb
+++ spec/controllers/sms_subscriptions_controller_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe SmsSubscriptionsController, :type => :controller do
+  let(:issue) { FactoryGirl.create(:issue) }
+  let(:issue2) { FactoryGirl.create(:issue) }
+
+  context 'subscribing to two issues' do
+    before do
+      post :create, {
+          from:     '16125551234',
+          sms_body: "suBscribe #{issue.code.downcase} #{issue2.code}"
+        }
+    end
+    it 'should work' do
+      expect(response.status).to eq(201)
+      expect(response.body.blank?).to be true
+      expect(assigns(:sms_user)).to_not be_nil
+      expect(assigns(:sms_user).reload.subscriptions.map(&:subscribable)).to eq [issue, issue2]
+    end
+
+
+    context 'and then unsubscribing' do
+      it 'should work' do
+        post :create, {
+            from:     '16125551234',
+            sms_body: "uNsuBscribe #{issue.code}"
+          }
+        expect(response.status).to eq(201)
+        expect(response.body.blank?).to be true
+        expect(assigns(:sms_user)).to_not be_nil
+        expect(assigns(:sms_user).reload.subscriptions.map(&:subscribable)).to eq [issue2]
+      end
+    end
+  end
+
+
+  context 'unsubscribing when not subscribed' do
+    it 'should work' do
+      post :create, {
+          from:     '16125551234',
+          sms_body: "uNsuBscribe #{issue.code}"
+        }
+      expect(response.status).to eq(202)
+      expect(response.body.blank?).to be false
+      expect(assigns(:sms_user)).to_not be_nil
+      expect(assigns(:sms_user).reload.subscriptions.map(&:subscribable)).to eq []
+    end
+  end
 
 end
