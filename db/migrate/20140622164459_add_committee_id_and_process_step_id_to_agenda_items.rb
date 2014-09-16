7944dfbbc7904e15708bea9a8f673c648082462a
Benjamin Rosas <ballPointPenguin@gmx.us>
2014-09-15 23:03:51 -0500

    restore event_id on agenda_items table

--- db/migrate/20140622164459_add_committee_id_and_process_step_id_to_agenda_items.rb
+++ db/migrate/20140622164459_add_committee_id_and_process_step_id_to_agenda_items.rb
@@ -3,5 +3,6 @@ class AddCommitteeIdAndProcessStepIdToAgendaItems < ActiveRecord::Migration
     add_reference :agenda_items, :process_step, index: true
     add_reference :agenda_items, :committee, index: true
     add_reference :agenda_items, :council_member, index: true
+    add_reference :agenda_items, :event, index: true
   end
 end
